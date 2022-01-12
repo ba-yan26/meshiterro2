36f1ca1d5f4716c67b9aea25e7b2ec805d46a8b6
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-01-12 02:55:19 +0000

    [Upload]5-6,deviseのヘルパーを使用したヘッダーの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_actionはコントローラーの全てのアクションが実行される前に何らかの処理を行う時に使用する
+  # if:はbefore_actionのオプションの一つ。値にメソッド名（:devise_controller）を指定することで戻り値がtrueであれば処理を実行する
+  
+  
+  protected
+  # privateは記述したコントローラー内でしか参照できない
+  # protectedは呼び出された他のコントローラーからも参照することができる
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    
+  end
 end

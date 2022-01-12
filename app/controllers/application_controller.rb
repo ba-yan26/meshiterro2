dfb04fd3d9fbddb77ce2ed3e668325faaf6d2d52
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-01-12 03:16:19 +0000

    [Upload]5-7,login.logoutごの遷移先ページの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
   # before_actionはコントローラーの全てのアクションが実行される前に何らかの処理を行う時に使用する
   # if:はbefore_actionのオプションの一つ。値にメソッド名（:devise_controller）を指定することで戻り値がtrueであれば処理を実行する
   
+  def after_sign_in_path_for(resource)
+    # サインイン後にどこに遷移するか
+    # after_sign_in_path_forはdevise側が用意しているメソッド
+    # deviseの初期設定はroot_pathになっているために遷移先を変更する必要がある
+    about_path
+  end
+  
+  def after_sign_out_path_for(sesource)
+    about_path
+  end
   
   protected
   # privateは記述したコントローラー内でしか参照できない

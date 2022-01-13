71aa54e806ddb639338c022081f3cda4e62e8068
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-01-13 05:17:47 +0000

    [Upload]5-19,いいね機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   resources :post_images, only: [:new, :index, :show, :create, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    # 単数形にするとURLに/:idを含まない
+    # コメント機能は一つのコメントに複数コメントできる使用だが
+    # いいね機能は一人ひとつしかいいねできないため、ユーザIDと投稿ID(post_image)がわかればいい
     resources :post_comments, only: [:create, :destroy]
     # 画像投稿に対してコメントをできるように、post_imageとpost_commentを親子関係にする
   end
